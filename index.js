234a0def07481c97ec0be4df20586b26c960cfeb
Johnnie Choi <johnniechoi@Johnnies-MacBook-Pro.local>
2016-11-17 09:12:52 -0500

    deploy to heruko what I got now. Getting variables from the UI API

--- index.js
+++ index.js
@@ -14,7 +14,6 @@ app.get('/working', function(req, res){ // listens for request on /api route
 
 
 app.get('/properties', function(req, res){ // listens for request on /api route
-
   request('http://mie.greenvilleonline.com/public_approve_query1.php', function (error, response, body) { // api url
     if (!error && response.statusCode === 200) {
       console.log('Go buy a house!');
@@ -27,6 +26,15 @@ app.get('/properties', function(req, res){ // listens for request on /api route
 app.get('/search', function(req, res){
   request.post({url: 'http://mie.greenvilleonline.com/public_approve_query1.php', form: {selldate: '11/07/2016'}}, function(err, httpResponse, body){
    res.send(body);
+  });
+});
+
+app.get('/zillow', function(req, res){
+  var address = req.query.address,
+  zipcode = req.query.zipcode;
+  console.log(address, zipcode)
+  request({url: 'http://www.zillow.com/webservice/GetSearchResults.htm?zws-id=X1-ZWz19hs5hlzz0r_493z4&address=' + address + '&citystatezip=' + zipcode}, function(err, httpResponse, body){
+    res.send(body);
   });
 });
 
