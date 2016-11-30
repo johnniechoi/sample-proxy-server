2c2c61b810e54252ada5882e59e33fea8462d520
Johnnie Choi <johnniechoi@Johnnies-MacBook-Pro.local>
2016-11-30 13:29:21 -0500

    updated the scrape from Greenville online Foreclosed data

--- index.js
+++ index.js
@@ -2,32 +2,10 @@ var express = require('express');
 var request = require('request');
 var app = express();
 var cors = require('cors');
+var jsdom = require("jsdom");
+var addressParser = require('parse-address');
 
 app.use(cors()); //allows overriding cross origin policy (use npm install if needed)
-
-// app.get('/working', function(req, res){ // listens for request on /api route
-//  console.log('working!');
-//  res.send('working!'); // if no errors, send the body of data back to front end
-// });
-
-/* PUT YOUR CODE BETWEEN COMMENTS */
-
-
-// app.get('/properties', function(req, res){ // listens for request on /api route
-//   request('http://mie.greenvilleonline.com/public_approve_query1.php', function (error, response, body) { // api url
-//     if (!error && response.statusCode === 200) {
-//       console.log('Go buy a house!');
-//       res.send(body); // if no errors, send the body of data back to front end
-//     }
-//   });
-//
-// });
-
-// app.get('/search', function(req, res){
-//   request.post({url: 'http://mie.greenvilleonline.com/public_approve_query1.php', form: {selldate: '11/07/2016'}}, function(err, httpResponse, body){
-//    res.send(body);
-//   });
-// });
 
 app.get('/zillow', function(req, res){
   var address = req.query.address,
@@ -38,15 +16,69 @@ app.get('/zillow', function(req, res){
   });
 });
 
-// This is the real API with the correct key !
-// app.get('/zillow', function(req, res){
-//   var address = req.query.address,
-//   zipcode = req.query.zipcode;
-//   console.log(address, zipcode)
-//   request({url: 'http://www.zillow.com/webservice/GetSearchResults.htm?zws-id=X1-ZWz19hch6rsa2z_5f91a&address=' + address + '&citystatezip=' + zipcode}, function(err, httpResponse, body){
-//     res.send(body);
-//   });
-// });
+app.get('/greenville-houses', function(req, res){
+  var selldate = req.query.search;
+
+  // looks like a get request like this works:
+  // http://mie.greenvilleonline.com/public_approve_query1.php?page=1&searchvar=1&search=12/05/2016
+  request('http://mie.greenvilleonline.com/public_approve_query1.php?page=1&searchvar=1&search=' + selldate,
+    function(err, httpResponse, body1){
+      request('http://mie.greenvilleonline.com/public_approve_query1.php?page=2&searchvar=1&search=' + selldate,
+        function(err, httpResponse, body2){
+          // parse body1 and body2
+          // send back as combined result
+          parseTableData(body1, function(body1Json){
+            parseTableData(body2, function(body2Json){
+              res.send(body1Json.concat(body2Json));
+            });
+          });
+        }
+      );
+    }
+  );
+});
+
+/**
+ * Takes raw response from greenville county website and scrapes out the
+ * house data
+ */
+function parseTableData(content, callback){
+  jsdom.env(
+    content,
+    ["http://code.jquery.com/jquery.js"],
+    function (err, window) {
+      // Get jquery
+      var $ = window.$;
+
+      // Process the data
+      var $tables = $('table');
+      var $dataTable = $($tables.get(6));
+      var $dataRows = $dataTable.find('.smallCopy');
+
+      // convert html to json objects with the case number as the objectId
+      var data = $dataRows.map(function(index, row){
+
+        var address = addressParser.parseLocation(row.cells.item(2).textContent);
+
+        return {
+          date: row.cells.item(0).textContent,
+          objectId: row.cells.item(1).textContent,
+          address: [address.number, address.prefix, address.street, address.type].filter(Boolean).join(' '),
+          zipcode: address.zip,
+          city: address.city,
+          state: address.state,
+          attorney: row.cells.item(3).textContent,
+          plaintiff: row.cells.item(4).textContent,
+          defendant: row.cells.item(5).textContent,
+          soldAmount: row.cells.item(6).textContent
+        }
+      }).toArray();
+
+      // return the data minus the header row
+      callback(data.slice(-10));
+    }
+  );
+}
 
 /* PUT YOUR CODE ABOVE THIS COMMENT */
 
