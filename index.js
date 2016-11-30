b3ed92841dd68b89890a9b1d0854106f11aa3336
Johnnie Choi <johnniechoi@Johnnies-MacBook-Pro.local>
2016-11-30 14:55:18 -0500

    commented out app.use(cors())

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ var addressParser = require('parse-address');
 
 // app.use(cors()); //allows overriding cross origin policy (use npm install if needed)
 
+
 app.get('/test', function(req, res){ // listens for request on /api route
  console.log('working!');
  res.send('working!'); // if no errors, send the body of data back to front end
